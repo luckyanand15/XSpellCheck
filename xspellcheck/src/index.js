9071596a2eee8e659517d833dbec0ef867c96125
luckyanand15 <luckyanand.741@gmail.com>
2024-12-02 23:30:54 +0530

    done

---
+++ xspellcheck/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SpellCheckApp from './SpellCheckApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SpellCheckApp/>
+  </React.StrictMode>
+);
